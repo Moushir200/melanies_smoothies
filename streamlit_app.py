7578e37373fee9b81bcfb17d1858f0c1a4b49027
Moushir200 <90405595+Moushir200@users.noreply.github.com>
2024-03-28 16:04:36 -0600

    Update streamlit_app.py
    Use Our fruit_chosen Variable in the API Call

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,6 +38,7 @@ if ingredients_list:
     #Convert List to String
     for fruit_chosen in ingredients_list:
         ingredients_string+=fruit_chosen+' '
+        st.subheader(fuirt_chosen + 'Nutrition infomration'
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
         fv_df=st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
